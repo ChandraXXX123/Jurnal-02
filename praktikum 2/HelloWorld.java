d5d67377159a35f02496c074614f5f407c0b5560
ChandraYudistiraBolangan <71163122+ChandraYudistiraBolangan@users.noreply.github.com>
2020-09-12 19:27:33 +0800

    HelloWord.java

---
+++ praktikum 2/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld{
+
+	public static void main(String[] args) {
+		System.out.println("Nama: Chandra");
+		System.out.println("Nim: D3IF-6706204037");
+		System.out.println("Kelas: 44-03");
+	
+	}
+}
